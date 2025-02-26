1c1c4685fcb617483f8cd9ed13706a4c4acf3c0b
Baibhav Tripathi <34443574+baibhavtripathi@users.noreply.github.com>
2025-02-26 23:02:34 +0530

    optimized to O(k*log(k))

--- Implementation/DivisorOfANumber.py
+++ Implementation/DivisorOfANumber.py
@@ -1,3 +1,5 @@
+import math
+
 class findDivisor:
     divisors = [1]
     def divisorsFun(self, n):
@@ -7,8 +9,17 @@ class findDivisor:
         self.divisors.append(n)
         return self.divisors
 
+    def optimizedDivisorsFun(self, n):
+        for i in range(2, int(math.sqrt(n))):
+            if n % i == 0:
+                self.divisors.append(i)
+                self.divisors.append(n//i)
+        self.divisors.append(n)
+        return sorted(self.divisors)
+
+
 if __name__ == '__main__':
     n = int(input('Enter a number: '))
     findDivisor = findDivisor()
-    divisorsList = findDivisor.divisorsFun(n)
+    divisorsList = findDivisor.optimizedDivisorsFun(n)
     print('Divisor of {} is {}'.format(n, divisorsList))
