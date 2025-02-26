245faac8f19f47f4cd64fdb1229ab1a7cf95b575
Baibhav Tripathi <34443574+baibhavtripathi@users.noreply.github.com>
2025-02-26 22:57:09 +0530

    modified to class

--- Implementation/DivisorOfANumber.py
+++ Implementation/DivisorOfANumber.py
@@ -1,7 +1,14 @@
-n = int(input('Enter a number: '))
-divisors = [1]
-for i in range(2, n):
-    if n % i == 0:
-        divisors.append(i)
-divisors.append(n)
-print('Divisor of {} is {}'.format(n, divisors))
+class findDivisor:
+    divisors = [1]
+    def divisorsFun(self, n):
+        for i in range(2, n):
+            if n % i == 0:
+                self.divisors.append(i)
+        self.divisors.append(n)
+        return self.divisors
+
+if __name__ == '__main__':
+    n = int(input('Enter a number: '))
+    findDivisor = findDivisor()
+    divisorsList = findDivisor.divisorsFun(n)
+    print('Divisor of {} is {}'.format(n, divisorsList))
